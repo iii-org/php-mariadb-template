107f7c7db12fcf1a76e451c31df932a5e5be50ce
Administrator <admin@example.com>
2020-11-17 15:40:24 +0800

    test

--- php/src/login.php
+++ php/src/login.php
@@ -101,7 +101,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 </head>
 <body>
     <div class="wrapper" style="margin: auto;">
-        <h2 style="text-align:center;">Login</h2>
+        <h2 style="text-align:center;">Login (brancher test-src)</h2>
         <p style="text-align:center;">Please fill in your credentials to login.</p>
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
